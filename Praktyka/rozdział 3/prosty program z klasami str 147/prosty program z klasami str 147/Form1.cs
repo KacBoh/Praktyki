58c7a36117d561aaea5fd0d8c421ace3a672ef58
KacBoh <kacperbohdziun@gmail.com>
2023-09-14 13:05:15 +0200

    zakończenie zadanie str 147

---
+++ Praktyka/rozdział 3/prosty program z klasami str 147/prosty program z klasami str 147/Form1.cs
@@ -0,0 +1,30 @@
+﻿namespace prosty_program_z_klasami_str_147
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dlugosc = talker.gadanie(textBox1.Text, (int)numericUpDown1.Value);
+            MessageBox.Show("Długość wiadomości: " + dlugosc);
+        }
+    }
+
+    class talker
+    {
+        public static int gadanie(string thingToSay, int numberOfTimes)
+        {
+            string finalstring = "";
+            for (int count = 0; count < numberOfTimes; count++)
+            {
+                finalstring += thingToSay + "\n";
+            }
+            MessageBox.Show(finalstring);
+            return finalstring.Length;
+        }
+    }
+}
---
+++ Praktyka/rozdział 3/prosty program z klasami str 147/prosty program z klasami str 147/Program.cs
@@ -0,0 +1,17 @@
+namespace prosty_program_z_klasami_str_147
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
