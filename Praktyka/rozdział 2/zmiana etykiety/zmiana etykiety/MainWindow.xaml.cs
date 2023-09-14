76285b70d52dd66793804dd2ce54361f9646b0a6
KacBoh <kacperbohdziun@gmail.com>
2023-09-14 10:59:54 +0200

    zakończenie aplikacjij do zmiany etykiety

---
+++ Praktyka/rozdział 2/zmiana etykiety/zmiana etykiety/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zmiana_etykiety
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (box.IsChecked == true)
+            {
+                if(Label.Text == "Text z lewej")
+                {
+                    Label.Text = "Text z prawej";
+                    Label.HorizontalAlignment = HorizontalAlignment.Right;
+                }
+                else
+                {
+                    Label.Text = "Text z lewej";
+                    Label.HorizontalAlignment = HorizontalAlignment.Left;
+                }
+            }
+            else
+            if (box.IsChecked == false)
+            {
+                Label.Text = "Zmienianie tekstu jest wyłączone";
+                Label.HorizontalAlignment = HorizontalAlignment.Center;
+            }
+        }
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
