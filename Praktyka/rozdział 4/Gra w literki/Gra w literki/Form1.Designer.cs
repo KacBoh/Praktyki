30a886c069fff92af9fbbb59bbe4887ec8dfe312
KacBoh <kacperbohdziun@gmail.com>
2023-09-15 13:59:45 +0200

    l

--- Praktyka/rozdział 4/Gra w literki/Gra w literki/Form1.Designer.cs
+++ Praktyka/rozdział 4/Gra w literki/Gra w literki/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            listBox1 = new ListBox();
+            statusStrip1 = new StatusStrip();
+            corretLabel = new ToolStripStatusLabel();
+            missedLabel = new ToolStripStatusLabel();
+            totalLabel = new ToolStripStatusLabel();
+            accuracyLabel = new ToolStripStatusLabel();
+            difficultyProgresBar = new ToolStripProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 159;
+            listBox1.Items.AddRange(new object[] { "" });
+            listBox1.Location = new Point(0, 0);
+            listBox1.MultiColumn = true;
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(800, 190);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { corretLabel, missedLabel, totalLabel, accuracyLabel, difficultyProgresBar });
+            statusStrip1.Location = new Point(0, 164);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 26);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // corretLabel
+            // 
+            corretLabel.Name = "corretLabel";
+            corretLabel.Size = new Size(119, 20);
+            corretLabel.Text = "Prawidłowych: 0 ";
+            // 
+            // missedLabel
+            // 
+            missedLabel.Name = "missedLabel";
+            missedLabel.Size = new Size(61, 20);
+            missedLabel.Text = "Błędy: 0";
+            // 
+            // totalLabel
+            // 
+            totalLabel.Name = "totalLabel";
+            totalLabel.Size = new Size(88, 20);
+            totalLabel.Text = "Wszystkie: 0";
+            // 
+            // accuracyLabel
+            // 
+            accuracyLabel.Name = "accuracyLabel";
+            accuracyLabel.Size = new Size(111, 20);
+            accuracyLabel.Text = "Dokładność 0%";
+            // 
+            // difficultyProgresBar
+            // 
+            difficultyProgresBar.Alignment = ToolStripItemAlignment.Right;
+            difficultyProgresBar.Maximum = 800;
+            difficultyProgresBar.Name = "difficultyProgresBar";
+            difficultyProgresBar.Size = new Size(100, 18);
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 800;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 190);
+            Controls.Add(statusStrip1);
+            Controls.Add(listBox1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Gra w literki";
+            KeyDown += Form1_KeyDown;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel corretLabel;
+        private ToolStripStatusLabel missedLabel;
+        private ToolStripStatusLabel totalLabel;
+        private ToolStripStatusLabel accuracyLabel;
+        private System.Windows.Forms.Timer timer1;
+        private ToolStripProgressBar difficultyProgresBar;
     }
 }
--- Praktyka/rozdział 4/Gra w literki/Gra w literki/Form1.cs
+++ Praktyka/rozdział 4/Gra w literki/Gra w literki/Form1.cs
@@ -1,10 +1,55 @@
-namespace Gra_w_literki
+﻿namespace Gra_w_literki
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        Stats stats = new Stats();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Koniec Gry");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+                difficultyProgresBar.Value = 800 - timer1.Interval;
+
+            }
+            else
+            {
+                stats.Update(false);
+            }
+
+            corretLabel.Text = "Prawidłowe: " + stats.Correct;
+            missedLabel.Text = "Błędy: " + stats.Missed;
+            totalLabel.Text = "Łącznie: " + stats.Total;
+            accuracyLabel.Text = "Dokładność: " + stats.Accuracy;
         }
     }
 }
