1d8e025867611b5e569eb0f7459ba63c53b54b0f
KacBoh <kacperbohdziun@gmail.com>
2023-09-15 12:42:45 +0200

    dokończenie zadania str 209

---
+++ Praktyka/rozdaiał 4/Bar Niechlujny Janek str 207/Bar Niechlujny Janek str 207/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bar_Niechlujny_Janek_str_207
+{
+    public partial class Form1 : Form
+    {
+        MenuMaker Menu;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MenuMaker Menu = new MenuMaker { Randomizer = new Random() };
+            label1.Text = Menu.GetMenuItem();
+            label2.Text = Menu.GetMenuItem();
+            label3.Text = Menu.GetMenuItem();
+            label4.Text = Menu.GetMenuItem();
+            label5.Text = Menu.GetMenuItem();
+        }
+    }
+}
