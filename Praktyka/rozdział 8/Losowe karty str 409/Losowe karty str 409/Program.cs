d8589efb34d2d961101fa452caf18e437a5c8f31
KacBoh <kacperbohdziun@gmail.com>
2023-09-20 12:48:51 +0200

    zakończenie lososania kart str 409

---
+++ Praktyka/rozdział 8/Losowe karty str 409/Losowe karty str 409/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Losowe_karty_str_409
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Pieć losowych kart: ");
+            List<Card> cards = new List<Card>();
+            for (int i = 0; i < 5; i++)
+            {
+                cards.Add(new Card((Suits) random.Next(4), (Values) random.Next(1,14)));
+                Console.WriteLine(cards[i].Name);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("te same karty potasowane:");
+            cards.Sort(new CardComparer_byValue());
+            foreach(Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+            Console.ReadKey();
+        }
+             
+    }
+    
+}
+    
