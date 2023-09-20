3379fb12951d2f61a6ee15c72ea9d6aa29c4b86a
KacBoh <kacperbohdziun@gmail.com>
2023-09-20 12:23:04 +0200

    zadanie kaczki str 403

---
+++ Praktyka/rozdział 8/Lista kaczek str 403/Lista kaczek str 403/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lista_kaczek_str_403
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck(){ Kind = KindOfDuck.Mallard, Size = 17},
+                new Duck(){ Kind = KindOfDuck.Muscovy, Size = 18},
+                new Duck(){ Kind = KindOfDuck.Decoy, Size = 15},
+            };
+
+            Console.WriteLine(ducks[1].CompareTo(ducks[2]));
+            Console.ReadLine();
+        }
+    }
+}
