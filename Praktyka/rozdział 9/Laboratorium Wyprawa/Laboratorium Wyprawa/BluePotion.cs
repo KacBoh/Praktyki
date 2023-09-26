e3f18b5fe07bd39e581f7b6bbb7db7c4b2e12830
KacBoh <kacperbohdziun@gmail.com>
2023-09-26 10:48:35 +0200

    Dodanie miecza i mikstur

---
+++ Praktyka/rozdział 9/Laboratorium Wyprawa/Laboratorium Wyprawa/BluePotion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_Wyprawa
+{
+    internal class BluePotion : Weapon, IPotion
+    {
+        private bool used = false;
+        public BluePotion(Game game, Point location):base(game, location) { }
+
+        public bool Used
+        {
+            get { return used; }
+        }
+
+        public override string Name
+        {
+            get { return "Niebieska mikstura"; }
+        }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            game.IncreasePlayerHealth(5, random);
+            used = true;
+        }
+
+    }
+}
---
+++ Praktyka/rozdział 9/Laboratorium Wyprawa/Laboratorium Wyprawa/Sword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_Wyprawa
+{
+    class Sword : Weapon
+    {
+        public Sword (Game game, Point location) : base (game, location) 
+        {
+
+        }
+
+        public override string Name { get { return "Miecz"; } }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            if (DamageEnemy(direction, 10, 3, random))
+                return;
+            if (DamageEnemy(direction++, 10, 3, random))
+                return;
+            if (DamageEnemy(direction--, 10, 3, random))
+                return;
+
+        }
+
+    }
+}
