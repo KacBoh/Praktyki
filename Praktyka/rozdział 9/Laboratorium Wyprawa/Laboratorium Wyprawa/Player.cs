9703fe572fbd2e5ccb60cb36a1da74f4a7303784
KacBoh <kacperbohdziun@gmail.com>
2023-09-26 09:42:34 +0200

    Dodanie klas mover i player

---
+++ Praktyka/rozdział 9/Laboratorium Wyprawa/Laboratorium Wyprawa/Player.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_Wyprawa
+{
+    class Player : Mover
+    {
+        private Weapon equippedWeapon;
+
+        public int HitPoints { get; private set; }
+
+        public List<Weapon> inventory = new Listy<Weapon>();
+        public IEnumerable<string> Weapons
+        {
+            get
+            {
+                List<strign> names = new List<strign>();
+                foreach (Weapon weapon in inventory)
+                {
+                    names.Add(weapon.Name);
+                }
+                return names;
+            }
+        }
+
+        public Player (Game game, Point location) : base (game, location)
+        {
+            HitPoints = 10;
+        }
+
+        public void IncreaseHealth(int health, Random random)
+        {
+            HitPoints += random.Next(1, health);
+        }
+
+        public void Equip(string weaponName)
+        {
+            foreach(Weapon weapon in inventory)
+            {
+                if(weapon.Name == weaponName)
+                {
+                    equippedWeapon = weapon;
+                }
+            }
+        }
+
+        public void Move(Direction direction)
+        {
+            base.location = Move(direction, game.Boundaries);
+            if (!game.WeaponInRoom.PickedUp)
+            {
+                if (Math.Abs(game.WeaponInRoom.Location.X - this.location.X) <= 10 && Math.Abs(game.WeaponInRoom.Location.Y - this.location.Y) <= 10)
+                {
+                    game.WeaponInRoom.PickUpWeapon();
+                    inventory.Add(game.WeaponInRoom);
+                    if (inventory.Count() <= 1)
+                        Equip(game.WeaponInRoom.Name);
+                }
+
+            }
+        }
+
+        public void Attack(Direction direction, Random random)
+        {
+            if(equippedWeapon != null)
+            {
+                equippedWeapon.Attack(direction,random);
+            }
+            if (equippedWeapon is IPotion)
+            {
+                Invetory.Remove(equippedWeapon);
+                equippedWeapon=null;
+            }
+        }
+
+
+
+
+    }
+}
