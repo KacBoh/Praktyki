360983bcd189dca8aaed442e3a6e3ccb7db0a5a1
KacBoh <kacperbohdziun@gmail.com>
2023-09-26 13:17:12 +0200

    p

---
+++ Praktyka/rozdział 10/U niechlujniego Janka 2.0/U niechlujniego Janka 2.0/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bar_Niechlujny_Janek_str_207
+{
+    internal class MenuMaker
+    {
+        public Random Randomizer;
+
+        string[] Meats = { "Wołowina", "Salami", "Indyk", "Szynka", "Karkówka" };
+        string[] Condiments = { "Żółta musztarda", "Musztarda miodowa", "Przyprawa", "Sos francuski" };
+        string[] Breads = { "Chleb ryżowy", "Chleb biały", "Chleb zbożowy", "Bułka" };
+        
+        public string GetMenuItem()
+        {
+            
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat + ", " + randomCondiment + ", " + randomBread;
+        }
+
+    }
+}
---
+++ Praktyka/rozdział 10/U niechlujniego Janka 2.0/U niechlujniego Janka 2.0/klasacs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bar_Niechlujny_Janek_str_207
+{
+    internal class MenuMaker
+    {
+        public Random Randomizer = new Random();
+
+        private List<String> Meats = new List<String>() { "Wołowina", "Salami", "Indyk", "Szynka", "Karkówka" };
+        private List<String> Condiments = new List<String>() { "Żółta musztarda", "Musztarda miodowa", "Przyprawa", "Sos francuski" };
+        private List<String> Breads = new List<String>() { "Chleb ryżowy", "Chleb biały", "Chleb zbożowy", "Bułka" };
+        public ObservableCollection<MenuItem> Menu { get; private set; }
+
+        public DateTime GeneratedDate { get; private set; } 
+        public int NumberOfItems { get; set; }
+
+        public MenuMaker() 
+        {
+            Menu = new ObservableCollection<MenuItem>();
+            NumberOfItems = 10;
+        }
+
+        public string CreateMenuItem()
+        {
+
+            string randomMeat = Meats[Randomizer.Next(Meats.Count)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Count)];
+            string randomBread = Breads[Randomizer.Next(Breads.Count)];
+            return randomMeat + ", " + randomCondiment + ", " + randomBread;
+        }
+
+        public void UpdateMenu()
+        {
+            Menu.Clear();
+            for(int i =0; i < NumberOfItems; i++)
+            {
+                Menu.Add(CreateMenuItem());
+            }
+            GeneratedDate = DateTime.Now;
+        }
+
+    }
+}
